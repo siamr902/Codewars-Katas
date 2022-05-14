4bc9b19a4c833829c587dad0f114c0d395b11ed8
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-14 16:01:45 -0400

    Create Simple string characters.js

---
+++ 7kyu/Simple string characters.js
@@ -0,0 +1,17 @@
+// // https://www.codewars.com/kata/5a29a0898f27f2d9c9000058
+
+function solve(s){
+ let arr = [];
+  let upper = /[A-Z]/g
+  let lower = /[a-z]/g
+  let numbers = /\d/g
+  let special = /[^A-Za-z0-9]/g
+  arr.push(s.match(upper).length)
+  arr.push(s.match(lower).length)
+  arr.push(s.match(numbers).length)
+  arr.push(s.match(special).length)
+  if (arr.length === null) {
+    return [];
+  }
+  return arr
+}

4197f522301fd3766a5c088f73e0c9fa7d42aed0
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-14 15:49:22 -0400

    Create Sum of two lowest positive integers.js

---
+++ 7kyu/Sum of two lowest positive integers.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014
+
+function sumTwoSmallestNumbers(numbers) {  
+  let sorted = numbers.sort((a,b) => a - b)
+  return sorted[0] + sorted[1]
+  
+}

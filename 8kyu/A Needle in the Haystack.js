fcdcf00ea84f23d82d2f53d27ed864a34728e3aa
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-14 15:52:09 -0400

    Create A Needle in the Haystack.js

---
+++ 8kyu/A Needle in the Haystack.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+function findNeedle(haystack) {
+  const index = haystack.indexOf('needle');
+  return `found the needle at position ${index}`
+}

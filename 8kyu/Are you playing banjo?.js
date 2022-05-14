0cc0ee83c9a90e72cb5c72c075b22e723f5e0247
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-14 15:59:05 -0400

    Create Are you playing banjo?.js

---
+++ 8kyu/Are you playing banjo?.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+function areYouPlayingBanjo(name) {
+  if (name.charAt(0) === 'R' || name.charAt(0) === 'r') {
+    return name + ' plays banjo';
+  }
+  return name + ' does not play banjo';
+}

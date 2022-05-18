1c30b83a3f48edda9139e76d8794c8fadf8995f3
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-17 22:07:19 -0400

    Delete Remove the minimum.js

--- 8kyu/Remove the minimum.js
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/563cf89eb4747c5fb100001b
-
-function removeSmallest(numbers) {
-  let newArr = numbers.slice()
-  let min = Math.min.apply(null, newArr);
-  let fix = newArr.splice(newArr.indexOf(min), 1);
-  return newArr;
-}

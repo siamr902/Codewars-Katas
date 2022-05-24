ffc5c85372788ded8e69ea1c47c57f435a430eed
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-24 19:10:06 -0400

    Create Don't give me five!.js

---
+++ 7kyu/Don't give me five!.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5813d19765d81c592200001a
+
+function dontGiveMeFive(start, end) {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (!i.toString().includes('5')) count++
+  }
+  return count
+}

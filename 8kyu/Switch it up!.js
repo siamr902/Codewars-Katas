9392e51ff0f977d42e0242e6f48f4079a6fd35fd
Siam Rahman <97770989+siamr902@users.noreply.github.com>
2022-05-24 18:31:32 -0400

    Create Switch it up!.js

---
+++ 8kyu/Switch it up!.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+function switchItUp(number){
+  return number === 0 ? 'Zero' : number === 1 ? 'One' : number === 2 ? 'Two' : number === 3 ? 'Three' : number === 4 ? 'Four' : number === 5 ? 'Five' : number === 6 ? 'Six' : number === 7 ? 'Seven' : number === 8 ? 'Eight' : 'Nine'
+}
